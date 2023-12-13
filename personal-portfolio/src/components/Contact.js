78ce8b47867362555fdcc405865d9816154176a4
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-12-13 13:10:17 +0300

    Added newsletter component

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 
 export const Contact = () => {
@@ -47,10 +49,16 @@ export const Contact = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
-                <img src={contactImg} alt="Contact Us"/>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+              }
+            </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
-                <div>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Get In Touch</h2>
                 <form onSubmit={handleSubmit}>
                   <Row>
@@ -78,8 +86,8 @@ export const Contact = () => {
                     }
                   </Row>
                 </form>
-              </div>
-           
+              </div>}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
