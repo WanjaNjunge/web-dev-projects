5d4ff6621b6ee554cb3b546eb9c98be4c7c4efaa
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-01-11 11:55:09 +0300

    Add about content

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -9,10 +9,10 @@ export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(500);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-  const period = 2000;
+  const toRotate = [ "QA Engineer", "Web Developer" ];
+  const period = 250;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -55,9 +55,9 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <span className="tagline">Hey There!</span>
+                <h1>{`I'm Wanja, a `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "QA Engineer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>A curious problem solver who loves both the precision of QA engineering and the creative freedom of web development. Combining these skills allows me to build exceptional digital experiences while ensuring their flawless functionality.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
